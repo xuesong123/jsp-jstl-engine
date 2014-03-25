098d70c2d8d9664a166eb49cc8f5664d476ddbdf
xuesong123 <xuesong.net@gmail.com>
2014-03-25 17:27:03 +0800

    modify

--- webapp/WEB-INF/lib/libcopy.bat
+++ webapp/WEB-INF/lib/libcopy.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
-copy "..\..\..\release\ayada-1.0.6.jar" "."
+copy "..\..\..\release\ayada-1.0.8.jar" "."
 copy "..\..\..\lib\javassist-3.11.0.GA.jar" "."
 copy "..\..\..\lib\libcopy.bat" "."
 copy "..\..\..\lib\logback-classic-0.9.24.jar" "."
 copy "..\..\..\lib\logback-core-0.9.24.jar" "."
 copy "..\..\..\lib\ognl-3.0.6.jar" "."
 copy "..\..\..\lib\slf4j-api-1.7.2.jar" "."
+copy "..\..\..\lib\mysql-connector-java-5.1.7.jar" "."
